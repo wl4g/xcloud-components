33a42bc16dc575f8215fad6e5ecff4089fefdedb
wanglsir <wangl@shangmaikeji.com>
2020-11-24 23:00:37 +0800

    fix: maven includes META-INF

---
+++ xcloud-components-rpc/xcloud-components-rpc-springcloud-dubbo/backup/analysis_dubbo/Protocol$Adaptive.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2017 ~ 2025 the original author or authors. <wanglsir@gmail.com, 983708408@qq.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.wl4g.iam.controller;
+
+import com.alibaba.dubbo.common.extension.ExtensionLoader;
+
+public class Protocol$Adaptive implements com.alibaba.dubbo.rpc.Protocol {
+	public com.alibaba.dubbo.rpc.Invoker refer(java.lang.Class arg0, com.alibaba.dubbo.common.URL arg1)
+			throws com.alibaba.dubbo.rpc.RpcException {
+		if (arg1 == null)
+			throw new IllegalArgumentException("url == null");
+		com.alibaba.dubbo.common.URL url = arg1;
+		String extName = (url.getProtocol() == null ? "dubbo" : url.getProtocol());
+		if (extName == null)
+			throw new IllegalStateException("Fail to get extension(com.alibaba.dubbo.rpc.Protocol) name from url("
+					+ url.toString() + ") use keys([protocol])");
+		com.alibaba.dubbo.rpc.Protocol extension = (com.alibaba.dubbo.rpc.Protocol) ExtensionLoader
+				.getExtensionLoader(com.alibaba.dubbo.rpc.Protocol.class).getExtension(extName);
+		return extension.refer(arg0, arg1);
+	}
+
+	public com.alibaba.dubbo.rpc.Exporter export(com.alibaba.dubbo.rpc.Invoker arg0) throws com.alibaba.dubbo.rpc.RpcException {
+		if (arg0 == null)
+			throw new IllegalArgumentException("com.alibaba.dubbo.rpc.Invoker argument == null");
+		if (arg0.getUrl() == null)
+			throw new IllegalArgumentException("com.alibaba.dubbo.rpc.Invoker argument getUrl() == null");
+		com.alibaba.dubbo.common.URL url = arg0.getUrl();
+		String extName = (url.getProtocol() == null ? "dubbo" : url.getProtocol());
+		if (extName == null)
+			throw new IllegalStateException("Fail to get extension(com.alibaba.dubbo.rpc.Protocol) name from url("
+					+ url.toString() + ") use keys([protocol])");
+		com.alibaba.dubbo.rpc.Protocol extension = (com.alibaba.dubbo.rpc.Protocol) ExtensionLoader
+				.getExtensionLoader(com.alibaba.dubbo.rpc.Protocol.class).getExtension(extName);
+		return extension.export(arg0);
+	}
+
+	public void destroy() {
+		throw new UnsupportedOperationException(
+				"method public abstract void com.alibaba.dubbo.rpc.Protocol.destroy() of interface com.alibaba.dubbo.rpc.Protocol is not adaptive method!");
+	}
+
+	public int getDefaultPort() {
+		throw new UnsupportedOperationException(
+				"method public abstract int com.alibaba.dubbo.rpc.Protocol.getDefaultPort() of interface com.alibaba.dubbo.rpc.Protocol is not adaptive method!");
+	}
+
+}
