95482ea04208be6faf8ff1a67b6947ce450fb363
heweijie <babaa1f4@163.com>
2021-01-15 18:04:12 +0800

    feat: add flexmark util

---
+++ xcloud-component-common/src/main/java/com/wl4g/component/common/md/FlexmarkUtil.java
@@ -0,0 +1,31 @@
+package com.wl4g.component.common.md;
+
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.util.ast.Node;
+import com.vladsch.flexmark.util.data.MutableDataSet;
+
+/**
+ * @author vjay
+ * @date 2021-01-15 11:46:00
+ */
+public class FlexmarkUtil {
+
+    public static String md2html(String md) {
+        MutableDataSet options = new MutableDataSet();
+
+        // uncomment to set optional extensions
+        //options.set(Parser.EXTENSIONS, Arrays.asList(TablesExtension.create(), StrikethroughExtension.create()));
+
+        // uncomment to convert soft-breaks to hard breaks
+        //options.set(HtmlRenderer.SOFT_BREAK, "<br />\n");
+
+        Parser parser = Parser.builder(options).build();
+        HtmlRenderer renderer = HtmlRenderer.builder(options).build();
+
+        // You can re-use parser and renderer instances
+        Node document = parser.parse(md);
+
+        return renderer.render(document);
+    }
+}
