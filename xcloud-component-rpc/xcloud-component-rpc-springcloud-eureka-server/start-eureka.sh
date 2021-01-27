887f7aa6c4939291c06a28660e3112e790a1e108
wanglsir <wangl@shangmaikeji.com>
2021-01-27 15:41:42 +0800

    fix: configs

--- xcloud-component-rpc/xcloud-component-rpc-springcloud-eureka-server/start-eureka.sh
+++ xcloud-component-rpc/xcloud-component-rpc-springcloud-eureka-server/start-eureka.sh
@@ -7,15 +7,15 @@ if [ "$1" == "--local" ]; then
   echo "Started eureka(local) successfully!"
 elif [ "$1" == "--cluster" ]; then
   echo "Starting for peer1..."
-  nohup java -jar ${EXEC_FILE} ${JVM_OPTS} --spring.profiles.active=ha,peer1 --logging.file.name=/mnt/disk1/log/eureka/eureka-ha,peer1.log 2>&1 >/dev/null &
+  nohup java -jar ${EXEC_FILE} ${JVM_OPTS} --spring.profiles.active=ha,peer1 2>&1 >/dev/null &
   sleep 2
 
   echo "Starting for peer2..."
-  nohup java -jar ${EXEC_FILE} ${JVM_OPTS} --spring.profiles.active=ha,peer2 --logging.file.name=/mnt/disk1/log/eureka/eureka-ha,peer2.log 2>&1 >/dev/null &
+  nohup java -jar ${EXEC_FILE} ${JVM_OPTS} --spring.profiles.active=ha,peer2 2>&1 >/dev/null &
   sleep 2
 
   echo "Starting for peer3..."
-  nohup java -jar ${EXEC_FILE} ${JVM_OPTS} --spring.profiles.active=ha,peer3 --logging.file.name=/mnt/disk1/log/eureka/eureka-ha,peer3.log 2>&1 >/dev/null &
+  nohup java -jar ${EXEC_FILE} ${JVM_OPTS} --spring.profiles.active=ha,peer3 2>&1 >/dev/null &
 
   echo "Starting eureka(peer1,peer2,peer3) completed!"
 else
