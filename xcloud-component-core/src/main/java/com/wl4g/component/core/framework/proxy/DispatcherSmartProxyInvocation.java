2b58e6681bd63f750933701146d39ce33eb174cf
wanglsir <wanglsir@gmail.com>
2021-06-16 14:30:56 +0800

    fix: bugfix invocation exception use cause

--- xcloud-component-core/src/main/java/com/wl4g/component/core/framework/proxy/DispatcherSmartProxyInvocation.java
+++ xcloud-component-core/src/main/java/com/wl4g/component/core/framework/proxy/DispatcherSmartProxyInvocation.java
@@ -18,8 +18,11 @@ package com.wl4g.component.core.framework.proxy;
 import static com.wl4g.component.common.collection.CollectionUtils2.safeList;
 import static com.wl4g.component.common.lang.Assert2.notNullOf;
 import static com.wl4g.component.common.reflect.ReflectionUtils2.makeAccessible;
+import static java.lang.String.format;
+import static java.util.Objects.isNull;
 import static java.util.stream.Collectors.toList;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.List;
 import java.util.function.Supplier;
@@ -37,49 +40,55 @@ import org.springframework.beans.factory.support.DefaultListableBeanFactory;
  */
 public class DispatcherSmartProxyInvocation extends AbstractDispatcherProxyInvocation {
 
-	protected final Supplier<Object> targetSuplier;
-	protected Object target; // typeof object(not FactoryBean)
+    protected final Supplier<Object> targetSuplier;
+    protected Object target; // typeof object(not FactoryBean)
 
-	public DispatcherSmartProxyInvocation(DefaultListableBeanFactory beanFactory, SmartProxyAutoConfiguration configurer,
-			String targetBeanName, Class<?> targetClass, Supplier<Object> targetSuplier) {
-		super(beanFactory, configurer, targetBeanName, targetClass);
-		this.targetSuplier = notNullOf(targetSuplier, "targetSuplier");
-	}
+    public DispatcherSmartProxyInvocation(DefaultListableBeanFactory beanFactory, SmartProxyAutoConfiguration configurer,
+            String targetBeanName, Class<?> targetClass, Supplier<Object> targetSuplier) {
+        super(beanFactory, configurer, targetBeanName, targetClass);
+        this.targetSuplier = notNullOf(targetSuplier, "targetSuplier");
+    }
 
-	@Override
-	public Object getTarget() {
-		// TODO use cache?
-		// e.g: but org.mybatis.spring.MapperFactoryBean#getObject()
-		// Returns a new object each time.
-		/**
-		 * Notes: do not use {@link BeanFactory#getBean(targetBeanName)} to get
-		 * the actual original object here, because it may have been overridden
-		 * and registered as a proxy object, and there will be a dead loop when
-		 * calling.
-		 */
-		return (target = targetSuplier.get());
-	}
+    @Override
+    public Object getTarget() {
+        // TODO use cache?
+        // e.g: but org.mybatis.spring.MapperFactoryBean#getObject()
+        // Returns a new object each time.
+        /**
+         * Notes: do not use {@link BeanFactory#getBean(targetBeanName)} to get
+         * the actual original object here, because it may have been overridden
+         * and registered as a proxy object, and there will be a dead loop when
+         * calling.
+         */
+        return (target = targetSuplier.get());
+    }
 
-	@Override
-	public Object doInvoke(final Object target, final Method method, final Object[] args) throws Throwable {
-		if (!method.isAccessible()) {
-			makeAccessible(method);
-		}
+    @Override
+    public Object doInvoke(final Object target, final Method method, final Object[] args) throws Throwable {
+        if (!method.isAccessible()) {
+            makeAccessible(method);
+        }
 
-		// Match all filters that support intercepting target methods.
-		List<SmartProxyFilter> matchedFilters = safeList(configurer.getProcessors(targetClass)).stream()
-				.filter(p -> p.supportMethodProxy(target, method, targetClass, args)).collect(toList());
+        // Match all filters that support intercepting target methods.
+        List<SmartProxyFilter> matchedFilters = safeList(configurer.getProcessors(targetClass)).stream()
+                .filter(p -> p.supportMethodProxy(target, method, targetClass, args)).collect(toList());
 
-		// Invoke target(actual) method.
-		Object result = null;
-		try {
-			result = new InvocationChain(matchedFilters).doInvoke(target, method, args);
-		} catch (Throwable ex) {
-			throw ex;
-		}
+        // Invoke target(actual) method.
+        Object result = null;
+        try {
+            result = new InvocationChain(matchedFilters).doInvoke(target, method, args);
+        } catch (Throwable ex) {
+            if (isNull(ex) || isNull(ex.getCause())) {
+                throw new InvocationTargetException(null,
+                        format("Failed to invocation target: %s, method: %s, args: %s", target, method, args));
+            } else {
+                // Solved: java.lang.reflect.InvocationTargetException-->null
+                throw ex.getCause();
+            }
+        }
 
-		log.debug("Invoked proxied origin method: {}, args:{}, return: {}", method, args, result);
-		return result;
-	}
+        log.debug("Invoked proxied origin method: {}, args:{}, return: {}", method, args, result);
+        return result;
+    }
 
 }
