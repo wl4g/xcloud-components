ff5367cca0bd603822fc707141c365840210fd58
wanglsir <wanglsir@gmail.com>
2021-06-18 10:12:04 +0800

    opt: JacksonUtils

--- xcloud-component-common/src/main/java/com/wl4g/component/common/serialize/JacksonUtils.java
+++ xcloud-component-common/src/main/java/com/wl4g/component/common/serialize/JacksonUtils.java
@@ -15,16 +15,7 @@
  */
 package com.wl4g.component.common.serialize;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JavaType;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectReader;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.wl4g.component.common.reflect.ResolvableType;
-import com.wl4g.component.common.reflect.TypeUtils2;
-
+import static com.wl4g.component.common.lang.Assert2.notNullOf;
 import static java.util.Objects.isNull;
 
 import java.io.File;
@@ -35,6 +26,20 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import javax.annotation.Nullable;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.wl4g.component.common.reflect.ResolvableType;
+import com.wl4g.component.common.reflect.TypeUtils2;
+
 /**
  * JACKSON utility tools.
  * 
@@ -44,235 +49,277 @@ import java.util.Map;
  */
 public abstract class JacksonUtils {
 
-	/**
-	 * Object to JSON strings.
-	 * 
-	 * @param object
-	 * @return
-	 */
-	public static String toJSONString(Object object) {
-		if (isNull(object))
-			return null;
-		try {
-			return defaultObjectMapper.writeValueAsString(object);
-		} catch (JsonProcessingException e) {
-			throw new IllegalArgumentException(e);
-		}
-	}
+    /**
+     * Object to JSON strings.
+     * 
+     * @param object
+     * @return
+     */
+    public static String toJSONString(@Nullable Object object) {
+        if (isNull(object)) {
+            return null;
+        }
+        try {
+            return defaultObjectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
 
-	/**
-	 * Parse object from JSON strings.
-	 * 
-	 * @param content
-	 * @param clazz
-	 * @return
-	 */
-	public static <T> T parseJSON(String content, Class<T> clazz) {
-		if (content == null) {
-			return null;
-		}
-		try {
-			return defaultObjectMapper.readValue(content, clazz);
-		} catch (IOException e) {
-			throw new IllegalArgumentException(e);
-		}
-	}
+    /**
+     * Parse object from JSON strings.
+     * 
+     * @param content
+     * @param clazz
+     * @return
+     */
+    public static <T> T parseJSON(@Nullable String content, @NotNull Class<T> clazz) {
+        notNullOf(clazz, "clazz");
+        if (isNull(content)) {
+            return null;
+        }
+        try {
+            return defaultObjectMapper.readValue(content, clazz);
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
 
-	/**
-	 * Parse object from JSON {@link InputStream}.
-	 * 
-	 * @param src
-	 * @param clazz
-	 * @return
-	 */
-	public static <T> T parseJSON(InputStream src, Class<T> clazz) {
-		if (isNull(src))
-			return null;
-		try {
-			return defaultObjectMapper.readValue(src, clazz);
-		} catch (IOException e) {
-			throw new IllegalArgumentException(e);
-		}
-	}
+    /**
+     * Parse object from JSON {@link InputStream}.
+     * 
+     * @param src
+     * @param clazz
+     * @return
+     */
+    public static <T> T parseJSON(@Nullable InputStream src, @NotNull Class<T> clazz) {
+        notNullOf(clazz, "clazz");
+        if (isNull(src)) {
+            return null;
+        }
+        try {
+            return defaultObjectMapper.readValue(src, clazz);
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
 
-	/**
-	 * Parse object from JSON {@link File}.
-	 * 
-	 * @param content
-	 * @param valueTypeRef
-	 * @return
-	 */
-	public static <T> T parseJSON(File src, Class<T> clazz) {
-		if (isNull(src))
-			return null;
-		try {
-			return defaultObjectMapper.readValue(src, clazz);
-		} catch (Exception e) {
-			throw new IllegalArgumentException(e);
-		}
-	}
+    /**
+     * Parse object from JSON {@link File}.
+     * 
+     * @param content
+     * @param valueTypeRef
+     * @return
+     */
+    public static <T> T parseJSON(@Nullable File src, @NotNull Class<T> clazz) {
+        notNullOf(clazz, "clazz");
+        if (isNull(src)) {
+            return null;
+        }
+        try {
+            return defaultObjectMapper.readValue(src, clazz);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
 
-	/**
-	 * Parse object from JSON {@link File}.
-	 * 
-	 * @param content
-	 * @param valueTypeRef
-	 * @return
-	 */
-	public static <T> T parseJSON(File src, TypeReference<T> valueTypeRef) {
-		if (isNull(src))
-			return null;
-		try {
-			return defaultObjectMapper.readValue(src, valueTypeRef);
-		} catch (Exception e) {
-			throw new IllegalArgumentException(e);
-		}
-	}
+    /**
+     * Parse object from JSON {@link File}.
+     * 
+     * @param content
+     * @param valueTypeRef
+     * @return
+     */
+    public static <T> T parseJSON(@Nullable File src, @NotNull TypeReference<T> valueTypeRef) {
+        notNullOf(valueTypeRef, "valueTypeRef");
+        if (isNull(src)) {
+            return null;
+        }
+        try {
+            return defaultObjectMapper.readValue(src, valueTypeRef);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
 
-	/**
-	 * Parse object from JSON {@link InputStream}.
-	 * 
-	 * @param content
-	 * @param valueTypeRef
-	 * @return
-	 */
-	public static <T> T parseJSON(InputStream src, TypeReference<T> valueTypeRef) {
-		if (isNull(src))
-			return null;
-		try {
-			return defaultObjectMapper.readValue(src, valueTypeRef);
-		} catch (Exception e) {
-			throw new IllegalArgumentException(e);
-		}
-	}
+    /**
+     * Parse object from JSON {@link InputStream}.
+     * 
+     * @param content
+     * @param valueTypeRef
+     * @return
+     */
+    public static <T> T parseJSON(@Nullable InputStream src, @NotNull TypeReference<T> valueTypeRef) {
+        notNullOf(valueTypeRef, "valueTypeRef");
+        if (isNull(src)) {
+            return null;
+        }
+        try {
+            return defaultObjectMapper.readValue(src, valueTypeRef);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
 
-	/**
-	 * Parse object from JSON strings.
-	 * 
-	 * @param content
-	 * @param valueTypeRef
-	 * @return
-	 */
-	public static <T> T parseJSON(String content, TypeReference<T> valueTypeRef) {
-		if (content == null) {
-			return null;
-		}
-		try {
-			return defaultObjectMapper.readValue(content, valueTypeRef);
-		} catch (Exception e) {
-			throw new IllegalArgumentException(e);
-		}
-	}
+    /**
+     * Parse object from JSON strings.
+     * 
+     * @param content
+     * @param valueTypeRef
+     * @return
+     */
+    public static <T> T parseJSON(@Nullable String content, @NotNull TypeReference<T> valueTypeRef) {
+        notNullOf(valueTypeRef, "valueTypeRef");
+        if (isNull(content)) {
+            return null;
+        }
+        try {
+            return defaultObjectMapper.readValue(content, valueTypeRef);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
 
-	/**
-	 * Convert value to target type.</br>
-	 * 
-	 * @see com.fasterxml.jackson.databind.ObjectMapper#convertValue(Object,
-	 *      Class)
-	 * @param <T>
-	 * @param bean
-	 * @param toType
-	 * @return
-	 */
-	public static <T> T convertBean(Object bean, Class<T> toType) {
-		return defaultObjectMapper.convertValue(bean, toType);
-	}
+    /**
+     * Parse object to {@link JsonNode}.
+     * 
+     * @param object
+     * @param extractPathExpr
+     * @return
+     */
+    public static JsonNode parseJsonNode(@Nullable String content, @Nullable String extractPathExpr) {
+        if (isNull(content)) {
+            return null;
+        }
+        try {
+            return defaultObjectMapper.readTree(content).requiredAt(extractPathExpr);
+        } catch (JsonProcessingException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
 
-	/**
-	 * Convert value to reference type.</br>
-	 * 
-	 * @see com.fasterxml.jackson.databind.ObjectMapper#convertValue(Object,
-	 *      TypeReference)
-	 * @param <T>
-	 * @param bean
-	 * @param typeRef
-	 * @return
-	 */
-	public static <T> T convertBean(Object bean, TypeReference<T> typeRef) {
-		return defaultObjectMapper.convertValue(bean, typeRef);
-	}
+    /**
+     * Convert value to target type.</br>
+     * 
+     * @see com.fasterxml.jackson.databind.ObjectMapper#convertValue(Object,
+     *      Class)
+     * @param <T>
+     * @param bean
+     * @param toType
+     * @return
+     */
+    public static <T> T convertBean(@Nullable Object bean, @NotNull Class<T> toType) {
+        notNullOf(toType, "toType");
+        if (isNull(bean)) {
+            return null;
+        }
+        return defaultObjectMapper.convertValue(bean, toType);
+    }
 
-	/**
-	 * Convert value to Java type.</br>
-	 * 
-	 * @see com.fasterxml.jackson.databind.ObjectMapper#convertValue(Object,
-	 *      JavaType)
-	 * @param <T>
-	 * @param bean
-	 * @param toJavaType
-	 * @return
-	 */
-	public static <T> T convertBean(Object bean, JavaType toJavaType) {
-		return defaultObjectMapper.convertValue(bean, toJavaType);
-	}
+    /**
+     * Convert value to reference type.</br>
+     * 
+     * @see com.fasterxml.jackson.databind.ObjectMapper#convertValue(Object,
+     *      TypeReference)
+     * @param <T>
+     * @param bean
+     * @param typeRef
+     * @return
+     */
+    public static <T> T convertBean(@Nullable Object bean, @NotNull TypeReference<T> valueTypeRef) {
+        notNullOf(valueTypeRef, "valueTypeRef");
+        if (isNull(bean)) {
+            return null;
+        }
+        return defaultObjectMapper.convertValue(bean, valueTypeRef);
+    }
 
-	/**
-	 * Deep cloning object with JSON serial-deserial.
-	 * 
-	 * @param obj
-	 * @return
-	 */
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public static <T> T deepClone(T obj) {
-		if (isNull(obj)) {
-			return null;
-		}
+    /**
+     * Convert value to Java type.</br>
+     * 
+     * @see com.fasterxml.jackson.databind.ObjectMapper#convertValue(Object,
+     *      JavaType)
+     * @param <T>
+     * @param bean
+     * @param toJavaType
+     * @return
+     */
+    public static <T> T convertBean(@Nullable Object bean, @NotNull JavaType toJavaType) {
+        notNullOf(toJavaType, "toJavaType");
+        if (isNull(bean)) {
+            return null;
+        }
+        return defaultObjectMapper.convertValue(bean, toJavaType);
+    }
 
-		ResolvableType resolver = ResolvableType.forClass(obj.getClass());
-		if (Collection.class.isAssignableFrom(obj.getClass())) {
-			ObjectReader reader = null;
-			ResolvableType[] generics = resolver.getGenerics();
-			if (!isNull(generics) && generics.length == 1) {
-				Class<?> clazz = generics[0].getRawClass();
-				if (!isNull(clazz)) {
-					reader = defaultObjectMapper.readerForListOf(clazz);
-				}
-			}
-			if (isNull(reader)) { // Fallback
-				Collection collect = (Collection) obj;
-				if (collect.isEmpty()) {
-					return obj;
-				}
-				Iterator<Object> it = collect.iterator();
-				if (it.hasNext()) {
-					reader = defaultObjectMapper.readerForListOf(it.next().getClass());
-				}
-			}
-			try {
-				return reader.readValue(toJSONString(obj));
-			} catch (JsonProcessingException e) {
-				throw new IllegalStateException(e);
-			}
-		} else if (Map.class.isAssignableFrom(obj.getClass())) {
-			Map map = (Map) obj;
-			Map cloneMap = new LinkedHashMap<>(map.size());
-			map.forEach((key, val) -> cloneMap.put(deepClone(key), deepClone(val)));
-			return (T) cloneMap;
-		} else if (TypeUtils2.isSimpleType(obj.getClass())) { // Simple Class
-			return obj;
-		}
+    /**
+     * Deep cloning object with JSON serial-deserial.
+     * 
+     * @param obj
+     * @return
+     */
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public static <T> T deepClone(@Nullable T obj) {
+        if (isNull(obj)) {
+            return null;
+        }
 
-		// Custom bean(obj field after recursion)
-		return (T) parseJSON(toJSONString(obj), obj.getClass());
-	}
+        ResolvableType resolver = ResolvableType.forClass(obj.getClass());
+        if (Collection.class.isAssignableFrom(obj.getClass())) {
+            ObjectReader reader = null;
+            ResolvableType[] generics = resolver.getGenerics();
+            if (!isNull(generics) && generics.length == 1) {
+                Class<?> clazz = generics[0].getRawClass();
+                if (!isNull(clazz)) {
+                    reader = defaultObjectMapper.readerForListOf(clazz);
+                }
+            }
+            if (isNull(reader)) { // Fallback
+                Collection collect = (Collection) obj;
+                if (collect.isEmpty()) {
+                    return obj;
+                }
+                Iterator<Object> it = collect.iterator();
+                if (it.hasNext()) {
+                    reader = defaultObjectMapper.readerForListOf(it.next().getClass());
+                }
+            }
+            try {
+                return reader.readValue(toJSONString(obj));
+            } catch (JsonProcessingException e) {
+                throw new IllegalStateException(e);
+            }
+        } else if (Map.class.isAssignableFrom(obj.getClass())) {
+            Map map = (Map) obj;
+            Map cloneMap = new LinkedHashMap<>(map.size());
+            map.forEach((key, val) -> cloneMap.put(deepClone(key), deepClone(val)));
+            return (T) cloneMap;
+        } else if (TypeUtils2.isSimpleType(obj.getClass())) { // Simple Class
+            return obj;
+        }
 
-	/**
-	 * Gets default {@link ObjectMapper}
-	 * 
-	 * @return
-	 */
-	public static final ObjectMapper getDefaultObjectMapper() {
-		return defaultObjectMapper;
-	}
+        // Custom bean(obj field after recursion)
+        return (T) parseJSON(toJSONString(obj), obj.getClass());
+    }
 
-	/**
-	 * Default {@link ObjectMapper} instance.
-	 */
-	private static final ObjectMapper defaultObjectMapper = new ObjectMapper();
+    /**
+     * Gets default {@link ObjectMapper}
+     * 
+     * @return
+     */
+    @NotNull
+    public static final ObjectMapper getDefaultObjectMapper() {
+        return defaultObjectMapper;
+    }
 
-	static {
-		getDefaultObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		getDefaultObjectMapper().configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-	}
+    /**
+     * Default {@link ObjectMapper} instance.
+     */
+    private static final ObjectMapper defaultObjectMapper = new ObjectMapper();
+
+    static {
+        getDefaultObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        getDefaultObjectMapper().configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+    }
 
 }
