23af134740d9448e2e169ddbd1c7bac30d961d8a
wanglsir <wanglsir@gmail.com>
2021-06-30 14:15:38 +0800

    opt: optimizing

--- xcloud-component-common/src/main/java/com/wl4g/component/common/lang/SystemUtils2.java
+++ xcloud-component-common/src/main/java/com/wl4g/component/common/lang/SystemUtils2.java
@@ -15,18 +15,6 @@
  */
 package com.wl4g.component.common.lang;
 
-import org.apache.commons.lang3.SystemUtils;
-
-import com.wl4g.component.common.log.SmartLogger;
-
-import java.io.File;
-import java.lang.management.ManagementFactory;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
-
 import static com.google.common.base.Charsets.UTF_8;
 import static com.google.common.hash.Hashing.md5;
 import static com.wl4g.component.common.lang.Assert2.hasText;
@@ -36,7 +24,21 @@ import static java.util.Collections.list;
 import static java.util.Collections.sort;
 import static java.util.Objects.isNull;
 import static java.util.Objects.nonNull;
-import static org.apache.commons.lang3.StringUtils.*;
+import static org.apache.commons.lang3.StringUtils.contains;
+import static org.apache.commons.lang3.StringUtils.isBlank;
+import static org.apache.commons.lang3.StringUtils.split;
+
+import java.io.File;
+import java.lang.management.ManagementFactory;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.lang3.SystemUtils;
+
+import com.wl4g.component.common.log.SmartLogger;
 
 /**
  * System utility tools
@@ -47,157 +49,157 @@ import static org.apache.commons.lang3.StringUtils.*;
  */
 public abstract class SystemUtils2 extends SystemUtils {
 
-	final private static SmartLogger log = getLogger(SystemUtils2.class);
+    final private static SmartLogger log = getLogger(SystemUtils2.class);
 
-	// --- OS platform info. ---
+    // --- OS platform info. ---
 
-	/**
-	 * Current operating system is Android.
-	 */
-	final public static boolean IS_ANDRIOD = isAndroid0();
+    /**
+     * Current operating system is Android.
+     */
+    public static final boolean IS_ANDRIOD = isAndroid0();
 
-	/**
-	 * Check whether the current operating system is Android.
-	 */
-	private static boolean isAndroid0() {
-		return System.getProperty("java.runtime.name", "").toLowerCase().contains("android");
-	}
+    /**
+     * Check whether the current operating system is Android.
+     */
+    private static boolean isAndroid0() {
+        return System.getProperty("java.runtime.name", "").toLowerCase().contains("android");
+    }
 
-	// --- Runtime system info. ---
+    // --- Runtime system info. ---
 
-	/**
-	 * Global unique host hardware MAC identification.
-	 */
-	final public static String DEFAULT_GLOBAL_HOST_SERIAL = defaultGlobalHostSerial0();
+    /**
+     * Local current application process ID
+     */
+    public static final String LOCAL_PROCESS_ID = localProcessId0();
 
-	/**
-	 * Global unique identity of current application.
-	 */
-	final public static String GLOBAL_APP_SERIAL = globalAppSerial0();
+    /**
+     * Global unique host hardware MAC identification.
+     */
+    public static final String GLOBAL_HOST_SERIAL = globalHostSerial0();
 
-	/**
-	 * Local current application process ID
-	 */
-	final public static String LOCAL_PROCESS_ID = localProcessId0();
+    /**
+     * Global unique identity of current application.
+     */
+    public static final String GLOBAL_APP_SERIAL = globalAppSerial0();
 
-	/**
-	 * Global unique identification of current application process
-	 */
-	final public static String GLOBAL_PROCESS_SERIAL = globalProcessSerial0();
+    /**
+     * Global unique identification of current application process
+     */
+    public static final String GLOBAL_PROCESS_SERIAL = globalProcessSerial0();
 
-	/**
-	 * (Default)Obtain local host hardware MAC address identity.
-	 * 
-	 * @return
-	 */
-	private static String defaultGlobalHostSerial0() {
-		// Gets MAC address information of network card
-		try {
-			List<NetworkInterface> nis = list(getNetworkInterfaces());
-			// Ascii dict sort.
-			sort(nis, (o1, o2) -> o1.getName().compareTo(o2.getName()));
-			byte[] mac = null;
-			for (NetworkInterface ni : nis) {
-				if (nonNull(ni) && !ni.isLoopback() && ni.isUp()) {
-					mac = ni.getHardwareAddress();
-					if (nonNull(mac)) {
-						break;
-					}
-				}
-			}
-			if (isNull(mac)) { // No configured network?
-				log.error("=>> Failed to get network card info. the OS not configured network or connected to the network?");
-				return "Unknown Network";
-			}
+    /**
+     * Obtain local application processId.
+     * 
+     * @return
+     */
+    private static String localProcessId0() {
+        return ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
+    }
 
-			StringBuffer sb = new StringBuffer(32);
-			for (int i = 0; i < mac.length; i++) {
-				if (i != 0) {
-					sb.append("-");
-				}
-				// Byte to integer
-				int temp = mac[i] & 0xff;
-				String str = Integer.toHexString(temp);
-				if (str.length() == 1)
-					sb.append("0" + str);
-				else
-					sb.append(str);
-			}
-			return sb.toString().toLowerCase().replaceAll("-", "");
-		} catch (SocketException e) {
-			throw new IllegalStateException(e);
-		}
+    /**
+     * (Default)Obtain local host hardware MAC address identity.
+     * 
+     * @return
+     */
+    private static String globalHostSerial0() {
+        // Gets MAC address information of network card
+        try {
+            List<NetworkInterface> nis = list(getNetworkInterfaces());
+            // Ascii dict sort.
+            sort(nis, (o1, o2) -> o1.getName().compareTo(o2.getName()));
+            byte[] mac = null;
+            for (NetworkInterface ni : nis) {
+                if (nonNull(ni) && !ni.isLoopback() && ni.isUp()) {
+                    mac = ni.getHardwareAddress();
+                    if (nonNull(mac)) {
+                        break;
+                    }
+                }
+            }
+            if (isNull(mac)) { // No configured network?
+                log.error("=>> Failed to get network card info. the OS not configured network or connected to the network?");
+                return "Unknown Network";
+            }
 
-	}
+            StringBuffer sb = new StringBuffer(32);
+            for (int i = 0; i < mac.length; i++) {
+                if (i != 0) {
+                    sb.append("-");
+                }
+                // Byte to integer
+                int temp = mac[i] & 0xff;
+                String str = Integer.toHexString(temp);
+                if (str.length() == 1)
+                    sb.append("0" + str);
+                else
+                    sb.append(str);
+            }
+            return sb.toString().toLowerCase().replaceAll("-", "");
+        } catch (SocketException e) {
+            throw new IllegalStateException(e);
+        }
 
-	/**
-	 * Obtain global application identity.
-	 * 
-	 * @return
-	 */
-	@SuppressWarnings("deprecation")
-	private static String globalAppSerial0() {
-		hasText(DEFAULT_GLOBAL_HOST_SERIAL, "HostSerial is empty.");
-		String packagePath = SystemUtils2.class.getProtectionDomain().getCodeSource().getLocation().toString();
-		return md5().hashString(DEFAULT_GLOBAL_HOST_SERIAL + packagePath, UTF_8).toString();
-	}
+    }
 
-	/**
-	 * Obtain local application processId.
-	 * 
-	 * @return
-	 */
-	private static String localProcessId0() {
-		return ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
-	}
+    /**
+     * Obtain global application identity.
+     * 
+     * @return
+     */
+    @SuppressWarnings("deprecation")
+    private static String globalAppSerial0() {
+        hasText(GLOBAL_HOST_SERIAL, "HostSerial is empty.");
+        String packagePath = SystemUtils2.class.getProtectionDomain().getCodeSource().getLocation().toString();
+        return md5().hashString(GLOBAL_HOST_SERIAL + packagePath, UTF_8).toString();
+    }
 
-	/**
-	 * Obtain global application identity.
-	 * 
-	 * @return
-	 */
-	private static String globalProcessSerial0() {
-		hasText(GLOBAL_APP_SERIAL, "LocalAppSerial is empty.");
-		hasText(LOCAL_PROCESS_ID, "LocalProcessId is empty.");
-		return GLOBAL_APP_SERIAL + String.format("%05d", Integer.parseInt(LOCAL_PROCESS_ID));
-	}
+    /**
+     * Obtain global application identity.
+     * 
+     * @return
+     */
+    private static String globalProcessSerial0() {
+        hasText(GLOBAL_APP_SERIAL, "LocalAppSerial is empty.");
+        hasText(LOCAL_PROCESS_ID, "LocalProcessId is empty.");
+        return GLOBAL_APP_SERIAL + String.format("%05d", Integer.parseInt(LOCAL_PROCESS_ID));
+    }
 
-	/**
-	 * System path invalid path cleanup. </br>
-	 * WINDOWS:
-	 * 
-	 * <pre>
-	 * cleanSystemPath("E:\\dir\\") == E:\dir
-	 * cleanSystemPath("E:\\log\\a.log\\") == E:\log\a.log
-	 * </pre>
-	 * 
-	 * UNIX:
-	 * 
-	 * <pre>
-	 * cleanSystemPath("/var/dir//") == /var/dir
-	 * cleanSystemPath("/var/log//a.log/") == /var/log/a.log
-	 * </pre>
-	 * 
-	 * @param systemPath
-	 * @return
-	 */
-	public static String cleanSystemPath(String systemPath) {
-		if (isBlank(systemPath) || !contains(systemPath, File.separator)) {
-			return systemPath;
-		}
-		// Clean invalid suffix path separator.
-		StringBuffer path = new StringBuffer();
-		Iterator<String> it = Arrays.asList(split(systemPath, File.separator)).iterator();
-		while (it.hasNext()) {
-			String part = it.next();
-			if (!isBlank(part)) {
-				path.append(part);
-				if (it.hasNext()) {
-					path.append(File.separator);
-				}
-			}
-		}
-		return path.toString();
-	}
+    /**
+     * System path invalid path cleanup. </br>
+     * WINDOWS:
+     * 
+     * <pre>
+     * cleanSystemPath("E:\\dir\\") == E:\dir
+     * cleanSystemPath("E:\\log\\a.log\\") == E:\log\a.log
+     * </pre>
+     * 
+     * UNIX:
+     * 
+     * <pre>
+     * cleanSystemPath("/var/dir//") == /var/dir
+     * cleanSystemPath("/var/log//a.log/") == /var/log/a.log
+     * </pre>
+     * 
+     * @param systemPath
+     * @return
+     */
+    public static String cleanSystemPath(String systemPath) {
+        if (isBlank(systemPath) || !contains(systemPath, File.separator)) {
+            return systemPath;
+        }
+        // Clean invalid suffix path separator.
+        StringBuffer path = new StringBuffer();
+        Iterator<String> it = Arrays.asList(split(systemPath, File.separator)).iterator();
+        while (it.hasNext()) {
+            String part = it.next();
+            if (!isBlank(part)) {
+                path.append(part);
+                if (it.hasNext()) {
+                    path.append(File.separator);
+                }
+            }
+        }
+        return path.toString();
+    }
 
 }
--- xcloud-component-common/src/test/java/com/wl4g/component/common/lang/SystemUtils2Tests.java
+++ xcloud-component-common/src/test/java/com/wl4g/component/common/lang/SystemUtils2Tests.java
@@ -20,7 +20,7 @@ import com.wl4g.component.common.lang.SystemUtils2;
 public class SystemUtils2Tests {
 
 	public static void main(String[] args) {
-		System.out.println(SystemUtils2.DEFAULT_GLOBAL_HOST_SERIAL);
+		System.out.println(SystemUtils2.GLOBAL_HOST_SERIAL);
 		System.out.println(SystemUtils2.GLOBAL_APP_SERIAL);
 		System.out.println(SystemUtils2.LOCAL_PROCESS_ID);
 		System.out.println(SystemUtils2.GLOBAL_PROCESS_SERIAL);
