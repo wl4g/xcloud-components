fefae14d70e598bcc76fbc71aac09599ee56220c
wanglsir <wanglsir@gmail.com>
2021-07-04 21:01:47 +0800

    fix: integration examples poms

--- xcloud-component-common-shade/src/main/java/com/wl4g/component/common/shade/None.java
+++ xcloud-component-common-shade/src/main/java/com/wl4g/component/common/shade/None.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.wl4g.components.common.shade;
+package com.wl4g.component.common.shade;
 
 /**
  * For correct parse of JavaDocs.
--- xcloud-component-common-shade/src/main/java/com/wl4g/component/common/shade/package-info.java
+++ xcloud-component-common-shade/src/main/java/com/wl4g/component/common/shade/package-info.java
@@ -13,4 +13,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.wl4g.components.common.shade;
+package com.wl4g.component.common.shade;
